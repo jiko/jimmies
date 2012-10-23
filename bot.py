3b04e696676f0eb3ba0604cd5f61534b14127eae
Jim <jk@jameskoppen>
2012-10-23 16:44:29 -0500

    Ready for primetime

--- bot.py
+++ bot.py
@@ -1,44 +1,51 @@
 #!/usr/bin/python
 import init_twit as tw
-import sys, time, re
-#import markov
-#markovLength = 3
-#source_file = '/home/jk/Code/jambot/corpora/corpus.txt'
+import time, re, random
 
-# optional command line parameters: path/to/corpus_file.txt n-gram_size
-if len (sys.argv) >= 2:
-	source_file = sys.argv[1]
-if len (sys.argv) == 3:
-	markovLength = int (sys.argv[2])
+lines = ["There is no need to be upset",
+	"My jimmies remain unrustled",
+	"Across the vast and majestic gulf of time and space the jimmies rustle softly",
+	"EVERYBODY'S GETTING RUSTLED",
+	"Did I just hear some rustling?"]
 
-if (markov.mapping=={}):
-	markov.buildMapping(markov.wordlist(source_file),markovLength)
+responses = [
+	"We must embrace our jimmies and burn them as fuel for our journey",
+	"Shh no tears only dreams now",
+	"Awaken child, and embrace the glory of your jimmies",
+	"http://www.youtube.com/watch?v=07wZiqJlu3U",
+	"http://www.youtube.com/watch?v=ygr5AHufBN4",
+	"http://www.youtube.com/watch?v=oScCgxuId98",
+	"http://www.youtube.com/watch?v=ygr5AHufBN4",
+	"http://www.youtube.com/watch?v=I0D1M3xneS8",
+	"http://www.youtube.com/watch?v=vBlX86yVEVs",
+	"http://www.youtube.com/watch?v=vaXcd5-BCTY",
+	"http://www.youtube.com/watch?v=MGayPWHjyIY",
+	"http://www.youtube.com/watch?v=Md7fY_ql0fg",
+	"http://www.youtube.com/watch?v=a6rFgflr5yo"
+	]
 
-def genTweet():
-	sentence = markov.genSentence(markovLength)
-	while (len (sentence) > 130 or len(sentence) < 30):
-		sentence = markov.genSentence(markovLength)
-	return sentence
+def genTweet(seq):
+	return random.choice(seq)
 
 while True:
 	results = tw.twitter.search(q="@"+tw.handle,since_id=tw.last_id_replied)['results']
 	if not results:
 		print "Nobody's talking to me...\n"
 	jimmies = tw.twitter.search(q="my jimmies",since_id=tw.last_id_replied)['results']
-	rustled = [jimi for jimi in jimmies if markov.re.search('rustl*',jimi['text'])]
+	rustled = [jimi for jimi in jimmies if re.search('rustl*',jimi['text'])]
 	results.extend(rustled)
 	for result in results:
 		question = result['text'].replace('@jmkp','')
 		asker = result['from_user']
 		status_id = str(result['id'])
 		print asker + " said '" + question + "'\n"
-		sentence = "We must embrace our jimmies and burn them as fuel for our journey" #genTweet()
+		sentence = genTweet(responses)
 		sentence = "@"+asker+" "+sentence
 		print status_id+": "+sentence+"\n"
 		if tw.last_id_replied < status_id:
 			tw.last_id_replied = status_id
 		#tw.poster.statuses.update(status=sentence,in_reply_to_status_id=status_id)
-	sentence = genTweet()
+	sentence = genTweet(lines)
 	print sentence+"\n"
 	#tw.poster.statuses.update(status=sentence)
 	print "Sweet Dreams...\n"
--- init_twit.py
+++ init_twit.py
@@ -4,11 +4,11 @@ from twitter.oauth_dance import oauth_dance
 import os
 
 # get your own at https://dev.twitter.com/apps/new
-CONSUMER_KEY='PUiHBJAy0QhCnpq3KytncA'
-CONSUMER_SECRET='9JVE1dOJOzx4HzPNByCaNSfkxWr5LbN1QOkOhJWZc'
+CONSUMER_KEY='bns94SYmy79jmQEOArj5g'
+CONSUMER_SECRET='YCkH7PocJ4zZAY9f5snyVjNBZRwJRcE5SEVEhaUZ8GQ'
 
 # path to where your oauth credentials are stored 
-oauth_filename = os.environ.get('HOME', '') + os.sep + '.twitter_oauth'
+oauth_filename = os.environ.get('HOME', '') + os.sep + '.rc_twitter_oauth'
 # use the function 'from twitter.oauth import write_token_file' to create this file
 oauth_token, oauth_token_secret = read_token_file(oauth_filename)
 
@@ -25,4 +25,4 @@ poster = Twitter(
 # get the status_id of the last tweet to which you replied
 last_id_replied = [tweet['in_reply_to_status_id'] for tweet in poster.statuses.user_timeline() if tweet['in_reply_to_status_id'] != None][0]
 # your bot's twitter handle
-handle = "jmkp"
+handle = "RustleCrowe"
