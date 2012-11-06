e431cb730c2a56bb26712b63bd9b7c0c27ab8f42
Jim <jk@jameskoppen>
2012-11-06 17:56:41 -0600

    Major changes to logging, tweet file organization and importing, and reply/tweet methods

--- bot.py
+++ bot.py
@@ -1,48 +1,59 @@
 #!/usr/bin/python
 import init_twit as tw
-import time, re, random
+import time, re, random, os
+
+def log(msg):
+	with open('log','a') as f:
+		f.write(msg+"\n")
+	print msg
 
 def fileToList(filename):
 	with open(filename) as f:
 		return [line.strip() for line in f]
 
-line_file = 'lines.txt'
-lines = fileToList(line_file)
+tweet_files = ['lines.txt','responses.txt','replies.txt']
+tweets = dict([ (os.path.splitext(name)[0], fileToList(name)) for name in tweet_files ])
 
-def genTweet(seq):
-	return random.choice(seq)
+def tweet(seq,irtsi=None,at=None): 
+	status = random.choice(tweets[seq])
+	try:
+		if at and irtsi:
+			status = "@"+at+" "+status
+			tw.poster.statuses.update(status=status,in_reply_to_status_id=irtsi)
+		else:
+			tw.poster.statuses.update(status=status)
+	except TwitterHTTPError as error:
+		log(error)
+	else:
+		if irtsi: 
+			status = "in reply to"+irtsi+": "+status
+		log(status)
+
+def reply(mention,seq):
+	asker = mention['from_user']
+	log(asker + " said " + mention['text'])
+	status_id = str(mention['id'])
+	if tw.last_id_replied < status_id:
+		tw.last_id_replied = status_id
+	tweet(seq,status_id,asker)
+
 
 while True:
-	sentence = genTweet(lines)
-	try:
-		tw.poster.statuses.update(status=sentence)
-	except TwitterHTTPError as error:
-		print error+"\n"
-	else:
-		print sentence+"\n"
+	rustled = re.compile('rt\s|rustl|thank|lo+l|laugh|hah|rofl|lmf?ao',flags=re.I)
 	results = tw.twitter.search(q="@"+tw.handle,since_id=tw.last_id_replied)['results']
-	jimmies = tw.twitter.search(q="my jimmies",since_id=tw.last_id_replied)['results']
-	rustled = [jimi for jimi in jimmies if re.search('rustl',jimi['text'],flags=re.I)]
-	if rustled:
-		results.append(rustled[0]) # Bulk unsolicited mentions will get your account suspended
-	unrustled = re.compile('rt\s|unrustl|thank|lo+l|laugh|hah|rofl|lmf?ao',flags=re.I)
-	results = [tweet for tweet in results if not unrustled.search(tweet['text'])]
+	results = [t for t in results if not rustled.search(t['text'])]
 	if results:
-		print "I detect a rustling in the jimmies...\n"
+		[reply(result,'replies') for result in results]
 	else:
-		print "Nobody's jimmies are rustled...\n"
-	for result in results:
-		asker = result['from_user']
-		print asker + " said " + result['text']
-		status_id = str(result['id'])
-		sentence = "@"+asker+" "+genTweet(lines)
-		if tw.last_id_replied < status_id:
-			tw.last_id_replied = status_id
-		try:
-			tw.poster.statuses.update(status=sentence,in_reply_to_status_id=status_id)
-		except TwitterHTTPError as error:
-			print error+"\n"
-		else:
-			print "in reply to "+status_id+": "+sentence+"\n"
-	print "Sweet Dreams...\n"
+		log("No replies necessary...")
+	rustled = re.compile('rustl',flags=re.I)
+	jimmies = tw.twitter.search(q="my jimmies",since_id=tw.last_id_replied)['results']
+	jimmies = [jimi for jimi in jimmies if rustled.search(jimi['text'])]
+	if jimmies:
+		log("I detect a rustling in the jimmies...")
+		reply(jimmies[0],"responses") # Bulk unsolicited mentions will get you suspended
+	else:
+		log("Nobody's jimmies are rustled...")
+	tweet('lines')
+	log("Sweet Dreams...")
 	time.sleep(7200) # waits for two hours
